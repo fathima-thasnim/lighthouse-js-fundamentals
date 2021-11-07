3f90b85aba14c03940fbde3de4f8640496b9cf20
fathima-thasnim <thasnimfathima492@gmail.com>
2021-11-07 17:52:04 +0000

    Hello world

---
+++ hello.js
@@ -0,0 +1,14 @@
+//anonymous function exercise 1
+// const sayHello = function (){
+//   console.log("Hello, world");
+// }
+// sayHello();
+
+//exercise 2
+const sayHello = function (name){
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
